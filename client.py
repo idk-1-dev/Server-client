e81380f187efb3c238451388a6aef0935c5e9758
user_1 <amitrog11@gmail.com>
2025-12-23 23:08:17 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#all the information on this project is in the README file on my github page
+
 import socket 
 import threading
 import os
@@ -6,7 +8,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT = "!DISC"
-SERVER = '127.0.0.1'
+SERVER = ''#put local or private ip here MUST BE THE SAME ON BOTH FILES
 ADDR = (SERVER, PORT)
 DISCALL = "!DISCALL"
 
